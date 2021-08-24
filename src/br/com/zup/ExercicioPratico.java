590eec56ee65985a2f9620647396027406c87d5b
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-24 16:03:07 -0300

    Exercício Prático

---
+++ src/br/com/zup/ExercicioPratico.java
@@ -0,0 +1,28 @@
+package br.com.zup;
+
+public class ExercicioPratico {
+    public static void main(String[] args) {
+
+        // Definindo as Variáveis
+        int numero1 = 20;
+        int numero2 = 20;
+        // Variável da operação de soma
+        int resultadoDaSoma = numero1 + numero2;
+        // Variável da operação de subtração
+        int resultadoDaSubtração = numero1 - numero2;
+        // Variável da operação de multiplicação
+        int resultadoDaMultiplicação = numero1 * numero2;
+        // Variável da operação de divisão
+        int resultadoDaDivisão = numero1 / numero2;
+
+        // Exibindo a operação de soma para o usuário
+        System.out.println("O resultado da soma é: " + resultadoDaSoma);
+        // Exibindo a operação de subtração para o usuário
+        System.out.println("O resultado da subtração é: " + resultadoDaSubtração);
+        // Exibindo a operação de multiplicação para o usuário
+        System.out.println("O resultado da multiplicação é: " + resultadoDaMultiplicação);
+        //Exibindo a operação de divisão para o usuário
+        System.out.println("O resultado da divisão é: " + resultadoDaDivisão);
+    }
+}
+
