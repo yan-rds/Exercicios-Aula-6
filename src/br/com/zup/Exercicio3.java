be825bf6f9b0ac515aaa4156e923ccd41542e5b9
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-24 16:19:53 -0300

    Exercício 3

---
+++ src/br/com/zup/Exercicio3.java
@@ -0,0 +1,14 @@
+package br.com.zup;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+
+        // Definindo variáveis
+        double metros = 4.8;
+        double centimetros = metros * 100;
+
+        // Dando essa conversão ao usuário
+        System.out.println(metros + " metros convertido para centímetros equivale a: " + centimetros + " centímetros.");
+
+    }
+}
