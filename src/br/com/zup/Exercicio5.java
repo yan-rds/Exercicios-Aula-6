d5d8444c5de89dcf1bf3c4f63415ce0b2a2267e6
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-24 16:33:35 -0300

    Exercício 5

---
+++ src/br/com/zup/Exercicio5.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+
+        // Definindo variáveis
+        double celsius = 28.5;
+        double fahrenheit = (celsius * 1.8) + 32;
+
+        // Informando a conversão ao usuário
+        System.out.println(celsius + "°C equivalem a " + fahrenheit + "°F.");
+    }
+}
