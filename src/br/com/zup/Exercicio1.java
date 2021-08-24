09908e615caf2bc16bc67c794e1ea90f34bb8a44
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-24 16:05:21 -0300

    Exercício 1

---
+++ src/br/com/zup/Exercicio1.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+        //Dizendo Hello, World!
+        System.out.println("Hello, World!");
+    }
+}
